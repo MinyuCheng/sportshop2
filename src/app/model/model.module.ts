9575cf2cf34a2c201471327ae053dbd5e8da7b15
Minyu Cheng <minyu.cheng@student.csulb.edu>
2020-05-05 12:27:46 -0700

    cart2

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -4,9 +4,11 @@ import { ProductRepository } from "./product.repository";
 
 import { StaticDataSource } from "./static.datasource";
 
+import { Cart } from "./cart.model";
+
 // make its classes/services available to other modules
 
 @NgModule({
-  providers: [ProductRepository, StaticDataSource],
+  providers: [ProductRepository, StaticDataSource, Cart],
 })
 export class ModelModule {}
