1be80ddb0fbf823bd52409dc79751ec3f6444277
Minyu Cheng <minyu.cheng@student.csulb.edu>
2020-05-05 12:46:06 -0700

    cart5

---
+++ src/app/store/cartDetail.component.ts
@@ -0,0 +1,2 @@
+import { Component } from '@angular/core'@Component({template: `<h3>Cart Detail Component</h3>`,})
+export class CartDetailComponent {}
---
+++ src/app/store/checkout.component.ts
@@ -0,0 +1,3 @@
+import { Component } from "@angular/core";
+@Component({ template: `<h3>CheckoutComponent</h3>` })
+export class CheckoutComponent {}
