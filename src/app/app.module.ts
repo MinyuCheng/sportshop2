9303067d1f7dfdfd248e5f370f5940495e1ec4be
Minyu Cheng <minyu.cheng@student.csulb.edu>
2020-05-05 14:35:27 -0700

    sf2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { RouterModule } from "@angular/router";
 import { StoreComponent } from "./store/store.component";
 import { CartDetailComponent } from "./store/cartDetail.component";
 import { CheckoutComponent } from "./store/checkout.component";
-//import { StoreFirstGuard } from "./storeFirst.guard";
+import { StoreFirstGuard } from "./storeFirst.guard";
 
 @NgModule({
   imports: [
@@ -16,22 +16,22 @@ import { CheckoutComponent } from "./store/checkout.component";
       {
         path: "store",
         component: StoreComponent,
-        //canActivate: [StoreFirstGuard],
+        canActivate: [StoreFirstGuard],
       },
       {
         path: "cart",
         component: CartDetailComponent,
-        //canActivate: [StoreFirstGuard],
+        canActivate: [StoreFirstGuard],
       },
       {
         path: "checkout",
         component: CheckoutComponent,
-        //canActivate: [StoreFirstGuard],
+        canActivate: [StoreFirstGuard],
       },
       { path: "**", redirectTo: "/store" },
     ]),
   ],
-  //providers: [StoreFirstGuard],
+  providers: [StoreFirstGuard],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
