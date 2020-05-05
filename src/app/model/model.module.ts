5388fa17bad15e48ad027f0d4f5744e352b7cd5b
Minyu Cheng <minyu.cheng@student.csulb.edu>
2020-05-05 12:09:50 -0700

    first commit

---
+++ src/app/model/model.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+
+import { ProductRepository } from "./product.repository";
+
+import { StaticDataSource } from "./static.datasource";
+
+// make its classes/services available to other modules
+
+@NgModule({
+  providers: [ProductRepository, StaticDataSource],
+})
+export class ModelModule {}
