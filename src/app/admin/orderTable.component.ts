c7df5565603193f95c45ee37e7b86cfd311fbaa3
Minyu Cheng <minyu.cheng@student.csulb.edu>
2020-05-07 16:47:21 -0700

    admin4

--- src/app/admin/orderTable.component.ts
+++ src/app/admin/orderTable.component.ts
@@ -1,27 +1,25 @@
-import { Component } from "@angular/core";
-import { Order } from "../model/order.model";
-import { OrderRepository } from "../model/order.repository";
+import { Component } from '@angular/core'
+import { Order } from '../model/order.model'
+import { OrderRepository } from '../model/order.repository'
 
 @Component({
-  templateUrl: "orderTable.component.html",
+  templateUrl: 'orderTable.component.html',
 })
 export class OrderTableComponent {
-  includeShipped = false;
+  includeShipped = false
 
   constructor(private repository: OrderRepository) {}
 
   getOrders(): Order[] {
-    return this.repository
-      .getOrders()
-      .filter((o) => this.includeShipped || !o.shipped);
+    return this.repository.getOrders().filter((o) => this.includeShipped || !o.shipped)
   }
 
   markShipped(order: Order) {
-    order.shipped = true;
-    this.repository.updateOrder(order);
+    order.shipped = true
+    this.repository.updateOrder(order)
   }
 
   delete(id: number) {
-    this.repository.deleteOrder(id);
+    this.repository.deleteOrder(id)
   }
 }
