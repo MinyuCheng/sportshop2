ad6d006689c46c58d5329c4d5c0424040476c623
Minyu Cheng <minyu.cheng@student.csulb.edu>
2020-05-07 16:36:11 -0700

    tables2

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,4 +1,18 @@
 import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
 
-@Component({ template: "<h3> Product Table PlaceHolder</h3>" })
-export class ProductTableComponent {}
+@Component({
+  templateUrl: "productTable.component.html",
+})
+export class ProductTableComponent {
+  constructor(private repository: ProductRepository) {}
+
+  getProducts(): Product[] {
+    return this.repository.getProducts();
+  }
+
+  deleteProduct(id: number) {
+    this.repository.deleteProduct(id);
+  }
+}
